7190f65862923b5cc0c331a51cc4682c861af568
周正 <zhouzheng@pool-100-2-0-70.nycmny.fios.verizon.net>
2016-10-28 14:46:33 +0800

    add new 3D touch view
    3D Touch 拖拽 新样式

---
+++ RXExtenstion/ViewController/User/Controller/RXNew3DTouchSettingController.m
@@ -0,0 +1,37 @@
+//
+//  RXNew3DTouchSettingController.m
+//  RXExtenstion
+//
+//  Created by zhouzheng on 16/10/28.
+//  Copyright © 2016年 https://github.com/srxboys. All rights reserved.
+//
+
+#import "RXNew3DTouchSettingController.h"
+
+@interface RXNew3DTouchSettingController ()
+
+@end
+
+@implementation RXNew3DTouchSettingController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

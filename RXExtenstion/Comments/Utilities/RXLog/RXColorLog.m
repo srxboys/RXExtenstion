3f0232250a7833dd0179465d5b3431e73153667a
孙蒙新 <sunmengxin@readtv.cn>
2017-03-21 11:22:05 +0800

    修改 RXColorLog 格式
    打印信息 格式修改
    1、更加直观
    2、copy 没有后缀 ` [; `
    3、也可再次封装 更改自己喜欢的样式

--- RXExtenstion/Comments/Utilities/RXLog/RXColorLog.m
+++ RXExtenstion/Comments/Utilities/RXLog/RXColorLog.m
@@ -21,23 +21,25 @@
     NSString *filePath = [[NSString stringWithUTF8String:file] lastPathComponent];
     NSString *LogHeader = [NSString stringWithFormat:@": \n   FILE->%@\n   LINE->%d\n   FUNCTION->",filePath, line];
     
+    format = [format stringByAppendingString:@"\n\n"];
+    
     if([self isEnableXcodeColors]) {
         if(isError) {
-            NSLog((@"%@   %@\n" XCODE_COLORS_ESCAPE @"fg255,253,56;" XCODE_COLORS_ESCAPE @"bg0,139,0;" @"%@" XCODE_COLORS_RESET @"\n\n"),LogHeader, method, format);
+            NSLog((@"%@ %@\n" XCODE_COLORS_ESCAPE @"fg255,253,56;" XCODE_COLORS_ESCAPE @"bg0,139,0;" @"%@" XCODE_COLORS_RESET),LogHeader, method, format);
         }
         else {
             if(LOG_ENABLE) {
-                NSLog((@"%@   %@\n" XCODE_COLORS_ESCAPE @"fg209,57,168;" @"%@" XCODE_COLORS_RESET @"\n\n"),LogHeader, method, format);
+                NSLog((@"%@ %@\n" XCODE_COLORS_ESCAPE @"fg209,57,168;" @"%@" XCODE_COLORS_RESET),LogHeader, method, format);
             }
         }
     }
     else {
         if(isError) {
-            NSLog(@"%@ %@ [ERROR❌] %@\n\n", method, method,format);
+            NSLog(@"%@ %@ [ERROR❌] %@", LogHeader, method,format);
         }
         else {
             if(LOG_ENABLE) {
-                NSLog(@"%@ %@ %@\n\n", method, method,format);
+                NSLog(@"%@ %@ %@", LogHeader, method,format);
             }
         }
         
